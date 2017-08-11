9154f8a6af1684c904ab088ab61a03641b642077
Vitaly Khabarov <vitkhab@gmail.com>
2017-08-11 14:59:37 +0300

    Initial commit

---
+++ scripts/00base.sh
@@ -0,0 +1,19 @@
+set -ux
+
+apk upgrade --no-cache --available
+
+source /etc/os-release
+
+cat << EOF > /etc/motd
+
+$PRETTY_NAME ($VERSION_ID) Base Image
+
+Built for use with Vagrant using:
+   <https://github.com/vitkhab-pro>
+
+See the Alpine Wiki for how-to guides and
+general information about administrating
+Alpine systems and development.
+See <http://wiki.alpinelinux.org>
+
+EOF
