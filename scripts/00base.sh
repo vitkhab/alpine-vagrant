60ba77f80e73df4de1375271e34481e9bed8639a
Vitaly Khabarov <vitkhab@gmail.com>
2018-07-21 20:39:59 +0300

    Fix repo url

--- scripts/00base.sh
+++ scripts/00base.sh
@@ -9,7 +9,7 @@ cat << EOF > /etc/motd
 $PRETTY_NAME ($VERSION_ID) Base Image
 
 Built for use with Vagrant using:
-   <https://github.com/vitkhab-pro>
+   <https://github.com/vitkhab>
 
 See the Alpine Wiki for how-to guides and
 general information about administrating
